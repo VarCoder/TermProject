5de85913e5e3a4bd98a417d0745225d8f3457133
VarCoder <bhat.varun50@gmail.com>
2022-07-29 14:52:52 -0400

    new commit lol

--- planar.py
+++ planar.py
@@ -18,22 +18,88 @@ shuffle the nodes and draw random paths between all nodes (that dont intersect)
 try to expand these paths to actually make a grid
 
 """
+import random
 
-class Graph():
-    def __init__(self,nodes):
-        self.nodes = nodes
-        self.adj_list = [set() for node in range(nodes+1)]
+
+# class Node(object):
+#     idToInstance = dict() #id -> 
+#     def __init__(self,id):
+#         self.id= id
+#         if id not in Node.idToInstance:
+#             self.x = None
+#             self.y = None
+#             Node.idToInstance[id] = self
+#         else:
+#             raise Exception("Node with id {id} already exists")
+        
+#     def assignXY(self,x_val,y_val):
+#         self.x = x_val
+#         self.y = y_val
+#     def __repr__(self):
+#         return str(self.num)
+#     def __hash__(self):
+#         return hash(self.num)
+#     def __eq__(self,other):
+#         return self.num == other.num
+
+# class Graph(object):
+#     def __init__(self, nodes):
+#         self.nodeCount = len(nodes)
+#         self.adjList = { n: set() for n in nodes }
+#     def initEdges(self):
+#         for key, value in self.adjList:
+#             value.add(Node(key.displayId + 1))
+#     def addEdge(self,source,dest):
+#         #destructivly modify adj_list so just return None
+#         if dest==source: return None
+#         #undirected graph so add both ways
+#         self.adjList[source].add(dest)
+#         self.adjList[dest].add(source)
+#     def getNodeAdjList(self,node):
+#         return self.adjList[node]
+#     def getAdjList(self):
+#         return self.adjList
+class Node(object):
+    def __init__(self,identifier):
+        self.ident = identifier
+        self.x = None
+        self.y = None
+    def __repr__(self):
+        return f"Node({self.ident})"
+    def __eq__(self,other):
+        return isinstance(other,Node) and self.ident==other.ident
+    def __hash__(self):
+        return hash(self.ident)
+class Graph(object):
+    def __init__(self,nodeCount):
+        self.n = nodeCount
+        self.nodes = [Node(i) for i in range(self.n)]
+        self.adjList = [set() for tmp in range(self.n)]
+
+    def idToNode(self,id):
+        return self.nodes[id]
     def initEdges(self):
-        for i in range(0,self.nodes):
-            self.adj_list[i].add(i+1)
+        for id in range(0,self.n-1):
+            self.adjList[id].add(self.idToNode(id+1))
     def addEdge(self,source,dest):
-        #destructivly modify adj_list so just return None
-        if dest==source: return None
-        #undirected graph so add both ways
-        self.adj_list[source].add(dest)
-        self.adj_list[dest].add(source)
+        if source == dest: return None
+        self.adjList[self.idToNode(source)].add(self.idToNode(dest))
 
 numNodes = 10
 G = Graph(numNodes)
 G.initEdges()
+print(G.adjList)
+"""Brainstorm:
+How should edges be randomly created?
+More is better than less
+limit the outdegree to be n/2
+choose nodes randomly"""
+# def createRandomEdges(maxOutdegree):
+#     #choose gap greater than 1
+#     for node in range(numNodes):
+#         newNodes = random.randrange(1,maxOutdegree)
+#         while len(G.getNodeAdjList(node)) <= newNodes:
+#             shift = random.randrange(-node,numNodes-node+1)
+#             G.addEdge(node,shift+node)
+startNode = 0 #fixed because always 0th node
 
