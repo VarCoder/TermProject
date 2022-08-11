e55e5faa3db0aa9faa3bce11d20058c209b82184
VarCoder <bhat.varun50@gmail.com>
2022-08-11 14:32:09 -0400

    help menu created

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from delaunayTriangulation import *
 from dualGraph import *
 from bowyerWatson import *
 from hamiltonianPath import *
-
 def generateLevel(numNodes,width,height):
 
     #do-while loop doesn't exist in python
@@ -34,7 +33,7 @@ def generateLevel(numNodes,width,height):
     return triangles,centerLines,ej
 
 def appStarted(app):
-    app.level, app.cells, app.edgeAdj = generateLevel(33,app.width,app.height)
+    app.level, app.cells, app.edgeAdj = generateLevel(30,app.width,app.height)
     app.delVerts = { vert for tri in app.level for vert in tri.vertices }
     app.voronoiVerts = { vert for edge in app.cells for vert in edge }
     app.curpath = []
@@ -42,9 +41,16 @@ def appStarted(app):
     app.prevPaths = []
     app.win = 0
     app.gameOver = False
+    app.help = False
+    # app.cityImage = app.loadImage('https://www.pinterest.com/pin/608971180850708564/')
+    # app.cityImage = app.scaleImage
 def keyPressed(app,event):
     if event.key == "r":
         appStarted(app)
+    if event.key == "e":
+        app.gameOver = True
+    if event.key == "i":
+        app.help = not app.help
 def mousePressed(app,event):
     if app.gameOver: return
     app.badRun = False
@@ -61,6 +67,10 @@ def mousePressed(app,event):
         app.voronoiVerts,
         key=lambda x: distance(x, closestDel),
     )
+    test = sorted(app.voronoiVerts, key = lambda x: distance(x, closestDel))
+    
+    if distance(test[2],closestDel) < distance(closestDel,location):
+        return 
     if closestDel not in app.curpath:
         if len(app.curpath) == 0:
             app.curpath.append(closestDel)
@@ -90,46 +100,60 @@ def mousePressed(app,event):
     #     # else:
     #         # print("...")
 def drawCells(app,canvas):
-    width = 400
-    height = 400
+
     for cell_ in app.cells:
+        # cell = cell_
         cell = [[0,0],[0,0]]
-        cell[0][0] = min(max(cell_[0][0],0),app.width)
-        cell[0][1] = min(max(cell_[0][1],0),app.height)
-        cell[1][0] = min(max(cell_[1][0],0),app.width)
-        cell[1][1] = min(max(cell_[1][1],0),app.height)
+        cell[0][0] = min(max(cell_[0][0],20),app.width)
+        cell[0][1] = min(max(cell_[0][1],20),app.height)
+        cell[1][0] = min(max(cell_[1][0],20),app.width)
+        cell[1][1] = min(max(cell_[1][1],20),app.height)
         canvas.create_line(cell[0][0],cell[0][1],cell[1][0],cell[1][1],fill="black",width=3)
 def drawTriangles(app,canvas):
     for triangle in app.level:
         for edge in triangle.edges:
-            edge[1][0],edge[1][1]
+            # edge[1][0],edge[1][1]
+            
             canvas.create_oval(edge[0][0]-5,edge[0][1]-5,edge[0][0]+5,edge[0][1]+5,fill="red",width=3)
             canvas.create_oval(edge[1][0]-5,edge[1][1]-5,edge[1][0]+5,edge[1][1]+5,fill="red",width=3)
 def drawPath(app,canvas):
     for ind in range(len(app.curpath)):
         point = app.curpath[ind]
         # print(point,point[0])
-        canvas.create_text(point[0]+7,point[1]+7,text=str(ind))
+        canvas.create_text(point[0]+7,point[1]+7,text=str(ind),fill="green",font="Helvetica 9 bold")
 def drawWin(app,canvas):
     if app.badRun:
         text = "Make a path you haven't made before"
     else:
         text = f"You have won {app.win} times" if app.win != 1 else f"You have won once"
-    canvas.create_text(0,0,anchor="nw",text=text)
+    canvas.create_text(0,0,anchor="nw",text=text,font = "Helvetica 11 bold")
 def drawGameEnd(app,canvas):
     canvas.create_text(app.width/2,app.height/2,text="Congrats, you have won the game!",font = "Helvetica 11 bold")
+def drawBackground(app,canvas):
+    canvas.create_rectangle(0,20,app.width,app.height,fill="#1338BE")
+def drawHelp(app,canvas):
+    canvas.create_text(app.width/2,app.height/2,text="This is the Travelling Salesman Game!",font = "Helvetica 9 bold")
+    canvas.create_text(app.width/2, app.height/2 + 20, text="The rules are to click on the red dots and construct a path that goes through every dot once",font = "Helvetica 8",anchor="center")
+    canvas.create_text(app.width/2, app.height/2 + 40, text="When you construct a path, you may not traverse over the same dot more than once!",font = "Helvetica 8",anchor="center")
+    canvas.create_text(app.width/2, app.height/2 + 60, text="You can delete the path by clicking on a node again, and you may not construct the same path twice",font = "Helvetica 8",anchor="center")
+    canvas.create_text(app.width/2, app.height/2 + 80, text="The winning-condition of the game is to draw as many paths as there are dots, good luck!",font = "Helvetica 8",anchor="center")
+    
 
 def redrawAll(app,canvas):
-    if app.win == len(app.delVerts):
-        app.gameOver = True
-        drawGameEnd(app,canvas)
-    drawCells(app,canvas)
-    drawTriangles(app,canvas)
-    drawPath(app,canvas)
-    drawWin(app,canvas)
+    if app.help:
+        drawHelp(app,canvas)
+    else:
+        drawBackground(app,canvas)
+        drawCells(app,canvas)
+        drawTriangles(app,canvas)
+        drawPath(app,canvas)
+        drawWin(app,canvas)
+        if app.win == len(app.delVerts) or app.gameOver:
+            app.gameOver = True
+            drawGameEnd(app,canvas)
     
 def main():
-    runApp(width=450,height=450)
+    runApp(width=600,height=600)
 if __name__ == "__main__":
     """Goals:
     perspective camera
